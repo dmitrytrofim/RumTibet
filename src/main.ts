b7d6a98c8c1676304d719cc2ae582675380e64d2
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-03-30 15:54:22 +0300

    +

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import '@/style.css';
 import App from '@/App.vue';
 import router from '@router/router';
 import components from '@components/index';
+import VueYtframe from 'vue3-ytframe';
 
 const app = createApp(App);
 
 components.forEach((component) => app.component(component.name, component));
 
+app.use(VueYtframe);
 app.use(router);
 
 app.mount('#app');
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -10,11 +10,39 @@
     </div>
    </v-container>
   </section>
+  <section>
+   <v-container>
+    <div class="">
+     <div class="">
+      <div
+       class="i-wrap w-[400px] aspect-[0.875] rounded-[24px] overflow-hidden"
+      >
+       <img class="i-full" src="../assets/img/about.png" alt="" />
+      </div>
+      <VueYtframe
+       class="w-[400px] aspect-[0.875] rounded-[24px]"
+       video-id="7NK_JOkuSVY"
+       :player-vars="{ autoplay: 0, listType: 'user_uploads' }"
+      />
+     </div>
+     <div class="flex flex-col items-start">
+      <p class="">о нашем походе</p>
+      <h2 class="">Исследуйте все горные массивы мира вместе с нами</h2>
+      <p class="">Исследуйте все горные массивы мира вместе с нами</p>
+      <v-button>Программа тура</v-button>
+     </div>
+    </div>
+   </v-container>
+  </section>
  </main>
 </template>
 
 <script lang="ts">
-export default {};
+export default {
+ data() {
+  return {};
+ },
+};
 </script>
 
 <style scoped></style>
