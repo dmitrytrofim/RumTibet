c5d6bafafb9e402be5d3785073cf544f098d2f7d
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-03-31 13:44:42 +0300

    +

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -203,6 +203,110 @@
     </div>
    </v-container>
   </section>
+  <section class="bg-[var(--b-light)]">
+   <v-container>
+    <div class="flex flex-col items-center py-[80px]">
+     <p class="overtitle mb-[10px]">делимся впечатлениями</p>
+     <h2 class="title mb-[40px]">Блог о путешествиях</h2>
+     <div class="grid grid-cols-2 gap-[20px] mb-[50px]">
+      <div
+       class="flex justify-between gap-[24px] bg-[var(--b-white)] rounded-[10px] p-[16px]"
+      >
+       <div class="i-wrap shrink-0 w-[230px] aspect-[0.82]">
+        <img class="i-full rounded-[6px]" src="/img/blog-1.png" alt="" />
+       </div>
+       <div class="flex flex-col py-[16px]">
+        <h3 class="text-24 leading-[1.3] mb-[24px]">
+         Красивая Италя, какая она в реальности?
+        </h3>
+        <p class="grow font-400 leading-[1.4]">
+         Для современного мира базовый вектор развития предполагает независимые
+         способы реализации соответствующих условий активизации.
+        </p>
+        <div class="flex justify-between">
+         <time class="text-[var(--t-orange)]" datetime="2023-04-01"
+          >01/04/2023</time
+         >
+         <a href="#" class="">читать статью</a>
+        </div>
+       </div>
+      </div>
+      <div
+       class="flex justify-between gap-[24px] bg-[var(--b-white)] rounded-[10px] p-[16px]"
+      >
+       <div class="i-wrap shrink-0 w-[230px] aspect-[0.82]">
+        <img class="i-full rounded-[6px]" src="/img/blog-1.png" alt="" />
+       </div>
+       <div class="flex flex-col py-[16px]">
+        <h3 class="text-24 leading-[1.3] mb-[24px]">
+         Красивая Италя, какая она в реальности?
+        </h3>
+        <p class="grow font-400 leading-[1.4]">
+         Для современного мира базовый вектор развития предполагает независимые
+         способы реализации соответствующих условий активизации.
+        </p>
+        <div class="flex justify-between">
+         <time class="text-[var(--t-orange)]" datetime="2023-04-01"
+          >01/04/2023</time
+         >
+         <a href="#" class="">читать статью</a>
+        </div>
+       </div>
+      </div>
+      <div
+       class="flex justify-between gap-[24px] bg-[var(--b-white)] rounded-[10px] p-[16px]"
+      >
+       <div class="i-wrap shrink-0 w-[230px] aspect-[0.82]">
+        <img class="i-full rounded-[6px]" src="/img/blog-1.png" alt="" />
+       </div>
+       <div class="flex flex-col py-[16px]">
+        <h3 class="text-24 leading-[1.3] mb-[24px]">
+         Красивая Италя, какая она в реальности?
+        </h3>
+        <p class="grow font-400 leading-[1.4]">
+         Для современного мира базовый вектор развития предполагает независимые
+         способы реализации соответствующих условий активизации.
+        </p>
+        <div class="flex justify-between">
+         <time class="text-[var(--t-orange)]" datetime="2023-04-01"
+          >01/04/2023</time
+         >
+         <a href="#" class="">читать статью</a>
+        </div>
+       </div>
+      </div>
+      <div
+       class="flex justify-between gap-[24px] bg-[var(--b-white)] rounded-[10px] p-[16px]"
+      >
+       <div class="i-wrap shrink-0 w-[230px] aspect-[0.82]">
+        <img class="i-full rounded-[6px]" src="/img/blog-1.png" alt="" />
+       </div>
+       <div class="flex flex-col py-[16px]">
+        <h3 class="text-24 leading-[1.3] mb-[24px]">
+         Красивая Италя, какая она в реальности?
+        </h3>
+        <p class="grow font-400 leading-[1.4]">
+         Для современного мира базовый вектор развития предполагает независимые
+         способы реализации соответствующих условий активизации.
+        </p>
+        <div class="flex justify-between">
+         <time class="text-[var(--t-orange)]" datetime="2023-04-01"
+          >01/04/2023</time
+         >
+         <a href="#" class="">читать статью</a>
+        </div>
+       </div>
+      </div>
+     </div>
+     <v-button>Другие материалы</v-button>
+    </div>
+   </v-container>
+  </section>
+  <section>
+   <v-container>
+    <div class=""></div>
+   </v-container>
+  </section>
  </main>
 </template>
 
